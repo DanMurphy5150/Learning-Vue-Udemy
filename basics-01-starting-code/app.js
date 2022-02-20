c22b7ef3650a5d037ce26655c296515e7e1d4e85
Daniel Murphy <daniel.murphy@capco.com>
2022-02-20 01:33:20 -0500

    added methods and called them in the html

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,12 +1,21 @@
 const app = Vue.createApp({
   data() {
     return {
-      message: 'Finish the course and learn Vue!',
+      messageA: 'Finish the course and learn Vue!',
+      messageB: 'Master Vue and build awesome apps!!',
       vueLink: 'https://vuejs.org/',
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.messageA;
+      } else {
+        return this.messageB;
+      }
+    },
   },
 });
 
 app.mount('#user-goal');
-
-// this is a commit test
