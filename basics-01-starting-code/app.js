2db0167bb0c3e93ba8b1da39aac1270722009b78
Daniel Murphy <daniel.murphy@capco.com>
2022-02-20 01:01:00 -0500

    Adding vue to test push

---
+++ basics-01-starting-code/app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      message: 'Finish the course and learn Vue!',
+      vueLink: 'https://vuejs.org/',
+    };
+  },
+});
+
+app.mount('#user-goal');
+
+// this is a commit test
