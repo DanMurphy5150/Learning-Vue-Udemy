bdc4f9900fee4476bf1bd6030e7844e672227ede
Daniel Murphy <daniel.murphy@capco.com>
2022-02-21 15:03:53 -0500

    Used watchers and computed and shorthand for v-on

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -3,8 +3,36 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
-      confirmedName: '',
+      // fullname: '',
+      lastName: '',
     };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 5000);
+      }
+    },
+    // name(value) {
+    //   console.log(value);
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = value + ' ' + 'Murphy';
+    //   }
+    // },
+  },
+  computed: {
+    fullname() {
+      console.log('running again');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + this.lastName;
+    },
   },
   methods: {
     add(num) {
@@ -13,7 +41,7 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
     },
-    setName(event, lastName) {
+    setName(event) {
       this.name = event.target.value;
     },
     submitForm() {
@@ -24,6 +52,12 @@ const app = Vue.createApp({
     },
     resetInput() {
       this.name = '';
+    },
+    outputFullname() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Murphy';
     },
   },
 });
