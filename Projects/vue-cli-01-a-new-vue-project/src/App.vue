671b12681eef2eb6d03affb01f2ca6b592db56c3
Daniel Murphy <daniel.murphy@capco.com>
2022-02-28 21:32:53 -0500

    Working on props

--- Projects/vue-cli-01-a-new-vue-project/src/App.vue
+++ Projects/vue-cli-01-a-new-vue-project/src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <section>
-    <h2>My Friends</h2>
+    <header><h1>My Friends</h1></header>
     <ul>
-      <friend-contact></friend-contact>
-      <friend-contact></friend-contact>
+      <friend-contact
+        name="Bobby Knight"
+        phone-number="321-434-1792"
+        email-address="Master_Bob@localhost.com"
+        is-favorite="1"
+      ></friend-contact>
+      <friend-contact
+        name="Bryce Donahue"
+        phone-number="654-666-5432"
+        email-address="dingusMan3000@localhost.com"
+        is-favorite="0"
+      ></friend-contact>
     </ul>
   </section>
 </template>
