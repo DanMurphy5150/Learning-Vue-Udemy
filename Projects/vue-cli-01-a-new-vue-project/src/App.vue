66b7e5003f58b60165b744575cc51bb5fbb97c07
Daniel Murphy <daniel.murphy@capco.com>
2022-02-28 20:40:20 -0500

    rebuilding My freinds app in Vue CLi project - got base project - needs styling

---
+++ Projects/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '321 434 9073',
+          email: 'manuel@localhost.com',
+        },
+        {
+          id: 'binky',
+          name: 'Binky Boy',
+          phone: '321 436 1792',
+          email: 'binky_boy5000@localhost.com',
+        },
+      ],
+    };
+  },
+};
+</script>
