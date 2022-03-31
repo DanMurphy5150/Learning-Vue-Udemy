ce211d8c5d01a8d4473b948594debb11cb6e233a
Daniel Murphy <daniel.murphy@capco.com>
2022-03-30 21:42:30 -0400

    added a root route and a catch all route

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -5,14 +5,17 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import CatchAllPage from './components/nav/CatchAllPage.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component: TeamsList },
+    { path: '/', redirect: '/teams' },
+    { path: '/teams', component: TeamsList }, // could add alias: '/' but this doesnt change the URL like in the first route
     { path: '/users', component: UsersList },
     // Order matters with routes
     { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:catchall(.*)', component: CatchAllPage },
   ],
 });
 
